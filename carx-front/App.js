b4b148a2dfb87da3c8f553ee9062d6d2aa55a1ea
= <=>
2021-10-29 14:17:20 +0100

    responsive ork

--- carx-front/App.js
+++ carx-front/App.js
@@ -1,27 +1,23 @@
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from "react-native";
 import tailwind from "tailwind-rn";
-import Test from "./src/components/test"
 
 export default function App() {
   return (
-    <View >
-      <View style={tailwind("h-full w-1/3 bg-blue-500 flex   ")}>
+    
+     <View style={tailwind("flex flex-row h-full  bg-gray-500 ")}>
+      <View style={tailwind(" w-1/3 bg-red-500 flex flex-col ")}>
+        <View style={tailwind(" h-1/3 bg-red-200 ")}><Text>hello</Text></View>
+      </View>
+      <View style={tailwind(" w-1/3 bg-blue-500  ")}>
         <Text>hello</Text>
-        <Test />
       </View>
     </View>
+    
   );
 }
 
 
 
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
