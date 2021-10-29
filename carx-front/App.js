8ce1ec73596edb474c83d2127a3fa9f5cb105651
firas-debich <debichf02@gmail.com>
2021-10-29 21:03:21 +0100

    log in 98 %

--- carx-front/App.js
+++ carx-front/App.js
@@ -6,20 +6,21 @@ import Navbar from './src/components/Navbar.js'
 import Test from './src/components/test.js'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import  LogIn from "./src/components/view1/login"
 const Stack = createNativeStackNavigator();
 
 
 export default function App() {
   return (
-    //test devide work
+    <>
+   
     <NavigationContainer>
       <Stack.Navigator>
       <Stack.Screen name="Nav" component={Navbar} />
       <Stack.Screen name="Test" component={Test} />  
       </Stack.Navigator>
     </NavigationContainer>
-    
+    </>
   );
 }
 
