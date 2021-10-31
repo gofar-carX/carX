3c66f157f8306848fdd6f8567ebe7a02a533dd08
oussema-yahyaoui <ousema.benabdallah.yahyaoui@gmail.com>
2021-10-31 16:04:29 +0100

    Profile and Editfile done 100%

--- carx-front/App.js
+++ carx-front/App.js
@@ -2,20 +2,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import tailwind from "tailwind-rn";
+// import Profile from './src/components/Profile'
+import ProfileEdit from './src/components/ProfileEdit';
 
 
 export default function App() {
   return (
     //test devide work
-     <View style={tailwind("flex flex-row h-full  bg-gray-500 ")}>
-      <View style={tailwind(" w-1/3 bg-red-500 flex flex-col ")}>
+    // <View style={tailwind("flex flex-row h-full  bg-gray-500 ")}>
+    <View>
+      {/* <Profile /> */}
+      <ProfileEdit />
+      {/* <View style={tailwind(" w-1/3 bg-red-500 flex flex-col ")}>
         <View style={tailwind(" h-1/3 bg-red-200 ")}><Text>hello</Text></View>
       </View>
       <View style={tailwind(" w-1/3 bg-blue-500  ")}>
         <Text>hello</Text>
-      </View>
+      </View> */}
     </View>
-    
+
   );
 }
 
