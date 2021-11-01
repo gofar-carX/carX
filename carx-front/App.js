a14721557c66b0ed6c5f78cccf9fdff565162ad1
firas-debich <debichf02@gmail.com>
2021-11-01 13:00:55 +0100

    merging the code

--- carx-front/App.js
+++ carx-front/App.js
@@ -5,7 +5,7 @@ import tailwind from "tailwind-rn";
 import SelectDropdown from 'react-native-select-dropdown'
 import Navbar from './src/components/Navbar.js'
 import Test from './src/components/test.js'
-import Login from './src/components/view1/login'
+import LogIn from './src/components/login/login.js';
 import Reviews from './src/components/Reviews'
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -13,20 +13,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Wash from './src/components/washService/PageOne/Wash.js';
 import HomeWash from './src/components/washService/HomeWash.js';
 import Confirmation from './src/components/washService/PageTow/Confirmation.js';
-
-
+import NavigationBar from "./src/components/Home/Home"
 const Stack = createNativeStackNavigator();
 
 
 export default function App() {
   const countries = ["Egypt", "Canada", "Australia", "Ireland"]
   return (
-  
+  // <NavigationBar/>
     <NavigationContainer>
       <Stack.Navigator>
+       <Stack.Screen name="Login" component={LogIn} />
       
-     
-      <Stack.Screen name="Login" component={Login} />
+
       <Stack.Screen name="Nav" component={Navbar} />
       <Stack.Screen name="Test" component={Test} />  
       </Stack.Navigator>
