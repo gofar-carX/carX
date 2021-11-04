93440fbb81980bb33dcbc3487b7b227e30fb85a7
= <=>
2021-11-04 10:27:40 +0100

    pull

--- carx-front/App.js
+++ carx-front/App.js
@@ -11,15 +11,17 @@ import Home from './src/components/Home/Home'
 import Profile from './src/components/Profile/Profile'
 import EditProfile from './src/components/Profile/ProfileEdit'
 
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
+import LocationPage from './src/components/washService/PageOne/Location'
 
 import Wash from './src/components/washService/PageOne/Wash.js';
 import HomeWash from './src/components/washService/HomeWash.js';
 import Confirmation from './src/components/washService/PageTow/Confirmation.js';
 import NavigationBar from "./src/components/Home/Home"
+import Maps from './src/components/washService/PageOne/Maps.js';
 const Stack = createNativeStackNavigator();
 
 
@@ -29,12 +31,13 @@ export default function App() {
   // <Wash />
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="Maps " component={Maps } /> 
+
       <Stack.Screen name="Wash" component={Wash} /> 
       <Stack.Screen name="Confirmation" component={Confirmation} /> 
       <Stack.Screen name="Login" component={LogIn} />
       <Stack.Screen name="Nav" component={Navbar} />
       <Stack.Screen name="Reviews" component={Reviews} /> 
-      <Stack.Screen name="Test" component={Test} /> 
      
       
 
