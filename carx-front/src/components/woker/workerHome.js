aaf5bbac158950f26928f9b5186161704f7b8c5a
mnawa3 <fouratir16@gmail.com>
2021-11-18 09:21:10 +0100

    pull

--- carx-front/src/components/woker/workerHome.js
+++ carx-front/src/components/woker/workerHome.js
@@ -9,6 +9,17 @@ const Stack = createNativeStackNavigator();
 export default function  WorkerHome(){
         
         useEffect(()=>{
+            (async () => {
+                let { status } = await Location.requestForegroundPermissionsAsync();
+                if (status !== "granted") {
+                  setErrorMsg("Permission to access location was denied");
+                  return;
+                }
+                let location = await Location.getCurrentPositionAsync({});
+                setLocation(location);
+                setid(user.id);
+                
+              })();  
             AsyncStorage.getItem("workerAuth").then((result)=>{
                 console.log(jwtDecode(result)["workerid"])
  
